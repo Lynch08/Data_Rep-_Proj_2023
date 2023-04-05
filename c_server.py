255faac831e7b4496550c751b85f233a4664c7fb
Enda Lynch <elyn@live.ie>
2023-04-06 00:22:56 +0100

    Flask

---
+++ c_server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     return "Hello World"
+
+@app.route ('/datainquery', methods = ['GET'])
+def inquery():
+     queryargs = {
+          "firstname" : request.args["firstname"],
+          "age" : request.args["age"]
+
+     }
+     print (queryargs)
+     return jsonify(queryargs)
+
+
+@app.route ('/datajson', methods = ['POST'])
+def asjson():
+     book = {
+          "title" : request.json["title"],
+          "author" : request.json["author"],
+          "price" : request.json["price"]
+
+     }
+
+     # put into data base
+     print (book)
+     return jsonify(book)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
